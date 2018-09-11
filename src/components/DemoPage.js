d6f62f5bfdc71323aa6c69a7279ad06a1d27e6c6
Nenad <baltic.nenad@gmail.com>
2018-09-11 15:11:27 +0200

    final2

--- src/components/DemoPage.js
+++ src/components/DemoPage.js
@@ -4,7 +4,7 @@ const DemoPage = () => {
     return (
         <div className="container">
             <div className="demo">
-                <h2>Admin part - just for the purpose of the presentation</h2>
+                <h2>Admin part - just for presentation</h2>
                 <img src="images/demo1.jpg" />
                 <img src="images/demo2.jpg" />
                 <img src="images/demo3.jpg" />
